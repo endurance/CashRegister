c0eaac43fdf506fe663284b79e1953c09885de18
Endurance Idehen <jeidehen@gmail.com>
2015-07-16 22:50:35 -0500

    Iteration 0

---
+++ CashRegisterWebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CashRegisterWebUI.Startup))]
+namespace CashRegisterWebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ Core/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CashRegister.Core.Models
+{
+    public class Product
+    {
+        public Guid Identifier { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string LongDescription { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
