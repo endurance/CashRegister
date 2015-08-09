40082e993af2d40f5caffba728e51aae215676ee
Endurance Idehen <jeidehen@gmail.com>
2015-08-09 14:23:10 -0500

    Added new ef project

---
+++ EntityFrameworkWithSqlLite/Main.cs
@@ -0,0 +1,16 @@
+﻿
+using CashRegister.Core.Models;
+
+namespace EntityFrameworkWithSqlLite
+{
+    static class MainProgram
+    {
+        static void Main(string[] args)
+        {
+            ItemDbContext context = new ItemDbContext();
+            context.Item.Add(new Item());
+            context.SaveChanges();
+            
+        }
+    }
+}
